60e61ae25990fb21089ecb9644070ae10ef993ca
doomsterinc <doomsterinc1@gmail.com>
2016-03-23 17:25:26 -0300

    addContatosFeatures2

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,7 +1,19 @@
+var contatos = [
+	{_id: 1, nome: 'contact example 1',
+	 email: 'cont1@empresa.com.br'},
+	 {_id: 2, nome: 'contact example 2',
+	 email: 'cont2@empresa.com.br'},
+	 {_id: 3, nome: 'contact example 3',
+	 email: 'cont3@empresa.com.br'}
+];
+
 module.exports = function() {
 	var controller = {}
 	controller.listaContatos = function (req,res) {
-
+		res.json(contatos);
+	};
+	controller.obtemContatos = function (req,res) {
+		console.log(req.params.id);
 	};
 	return controller;
 };
