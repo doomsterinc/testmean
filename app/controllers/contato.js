42ba64baa2a0e41ae137ad959e8c009b427865e9
doomsterinc <doomsterinc1@gmail.com>
2016-03-23 18:04:15 -0300

    addContatosFeatures3

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -13,7 +13,13 @@ module.exports = function() {
 		res.json(contatos);
 	};
 	controller.obtemContatos = function (req,res) {
-		console.log(req.params.id);
+		var idContato = req.params.id;
+		var contato = contatos.filter(function (contato){
+			return contato._id == idContato;
+		})[0];
+		contato ?
+		 res.json(contato) : 
+		 res.status(404).send('Contato não encontrado');
 	};
 	return controller;
 };
--- app/routes/contato.js
+++ app/routes/contato.js
@@ -1,5 +1,5 @@
 module.exports = function (app){
 	var controller = app.controllers.contato;
 	app.get('/contatos', controller.listaContatos);
-	app.get('/contatos/:id', controller.obtemContato);
+	app.get('/contatos/:id', controller.obtemContatos);
 };
